78c022de325e5fc0d2818896147b9fe69b585dbf
アーキテクトチーム <submit@inte.co.jp>
2013-07-31 17:16:29 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_repository_with_last_update do
+  name 'Redmine Repository With Last Update'
+  author 'y.yoshida'
+  description 'replace age with last update.'
+  version '0.0.1'
+  url 'https://github.com/yoshidayo/redmine_repository_with_last_update'
+  author_url 'http://www.ibs.inte.co.jp/'
+end
